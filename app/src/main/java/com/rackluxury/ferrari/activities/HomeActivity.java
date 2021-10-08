b5a944f4fa817c7af7d9e8e4eb9bc7949aecf445
HarshAProgrammer <App.Lavishly@Gmail.com>
2021-10-08 23:01:37 +0530

    rep

--- app/src/main/java/com/rackluxury/ferrari/activities/HomeActivity.java
+++ app/src/main/java/com/rackluxury/ferrari/activities/HomeActivity.java
@@ -702,10 +702,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
 
                 GeneralShareMainIntent.setType("Text/plain");
-                String generalMainShareBody = "Rolex:A Crown For Every Achievement      " +
+                String generalMainShareBody = "Ferrari:A Crown For Every Achievement      " +
                         "" +
                         appLink;
-                String generalMainShareSub = "Rolex App";
+                String generalMainShareSub = "Ferrari App";
                 GeneralShareMainIntent.putExtra(Intent.EXTRA_SUBJECT, generalMainShareSub);
                 GeneralShareMainIntent.putExtra(Intent.EXTRA_TEXT, generalMainShareBody);
                 startActivity(Intent.createChooser(GeneralShareMainIntent, "Share Via"));
@@ -803,7 +803,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     shareOnTwitterIntent.setType("text/plain");
                     final String appPackageName = getApplicationContext().getPackageName();
                     ShareOnTwitterString = "https://play.google.com/store/apps/details?id=" + appPackageName;
-                    shareOnTwitterIntent.putExtra(Intent.EXTRA_TEXT, "Rolex:A Crown For Every Achievement      " +
+                    shareOnTwitterIntent.putExtra(Intent.EXTRA_TEXT, "Ferrari:A Crown For Every Achievement      " +
                             "" + ShareOnTwitterString);
                     shareOnTwitterIntent.setPackage("com.twitter.android");
                     startActivity(shareOnTwitterIntent);
